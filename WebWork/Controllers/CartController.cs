d37ffa52e867e275f91c7c5c7f987951832f4f5a
sumkin91 <sumkin91@yandex.ru>
2022-05-20 17:58:54 +0300

    Добавлены представления Блогов, Каталога и Корзины

---
+++ WebWork/Controllers/CartController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers;
+
+public class CartController : Controller
+{
+    public IActionResult Index() => View();
+}
---
+++ WebWork/Controllers/CatalogController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers;
+
+public class CatalogController : Controller
+{
+    public IActionResult Index() => View(); // shop.html
+}
